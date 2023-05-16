8a9608694a851a7ec77f046928e3bd48115764d5
Jheinember Jimenez <jheinember.jimenez@peaku.io>
2023-05-16 10:44:54 -0400

    Add styles

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,20 +3,22 @@ import styles from "../Styles/Home.module.css";
 // import profile from "../Styles/Images/profile.jpg";
 import Profile from "../Components/Profile";
 import { RiLinkedinFill, RiGithubFill, RiDownload2Fill } from "react-icons/ri";
-import CV from "../Styles/Images/CV.pdf";
+import CV from "../Styles/Images/Portafolio-Lourdes-Lopez-Pepa-2023.pdf";
+import Carousel from "./Carousel";
 
 function Home() {
+  const strings = ["Full Stack Developer", "Graphic Designer"];
   return (
     <div className={styles.homeContenedor}>
       <div className={styles.imageBorder}>
         <Profile></Profile>
         {/* <img src={profile} alt="profile pic" className={styles.profilePic} /> */}
       </div>
-
       <div className={styles.homeRight}>
         <div className={styles.hola}>¡Hola!</div>
         <div className={styles.titulo}>Soy Lourdes López Pepa</div>
-        <div className={styles.subtitulo}>Full Stack Developer</div>
+        <Carousel strings={strings} interval={3000}></Carousel>
+        {/* <div className={styles.subtitulo}>Full Stack Developer</div> */}
         <div className={styles.descripcion}>
           Full Stack Developer con formación como Diseñadora Gráfica. Con
           pensamiento creativo, resolución de problemas, trabajo en equipo,
